cd01c43d2351a7e1f20b621854f6094d34476ca8
arogya <arogyabichpuria16@gmail.com>
2022-05-09 17:12:33 +0530

    added unresponsive lerp scroll

---
+++ assets/js/animation.js
@@ -0,0 +1,151 @@
+import gsap from "gsap";
+
+gsap.set(".follower", { xPercent: -50, yPercent: -50 });
+gsap.set(".cursor", { xPercent: -50, yPercent: -50 });
+
+let follow = document.querySelector(".follower");
+let cur = document.querySelector(".cursor");
+
+window.addEventListener("mousemove", (e) => {
+  gsap.to(cur, { duration: 0.2, x: e.clientX, y: e.clientY });
+  gsap.to(follow, { duration: 0.8, x: e.clientX, y: e.clientY });
+});
+
+/*--------------------
+Vars
+--------------------*/
+const $menu = document.querySelector(".menu");
+const $items = document.querySelectorAll(".menu--item");
+
+let menuHeight = $menu.clientHeight;
+let itemHeight = $items[0].clientHeight;
+let wrapHeight = $items.length * itemHeight;
+
+let scrollSpeed = 0;
+let oldScrollY = 0;
+let scrollY = 0;
+let y = 0;
+
+/*--------------------
+Lerp
+--------------------*/
+const lerp = (v0, v1, t) => {
+  return v0 * (1 - t) + v1 * t;
+};
+
+/*--------------------
+Dispose
+--------------------*/
+const dispose = (scroll) => {
+  gsap.set($items, {
+    y: (i) => {
+      return i * itemHeight + scroll;
+    },
+    modifiers: {
+      y: (y) => {
+        const s = gsap.utils.wrap(
+          -itemHeight,
+          wrapHeight - itemHeight,
+          parseInt(y)
+        );
+        return `${s}px`;
+      },
+    },
+  });
+};
+dispose(0);
+
+/*--------------------
+Wheel
+--------------------*/
+const handleMouseWheel = (e) => {
+  scrollY -= e.deltaY;
+};
+
+/*--------------------
+Touch
+--------------------*/
+let touchStart = 0;
+let touchY = 0;
+let isDragging = false;
+const handleTouchStart = (e) => {
+  touchStart = e.clientY || e.touches[0].clientY;
+  isDragging = true;
+  $menu.classList.add("is-dragging");
+};
+const handleTouchMove = (e) => {
+  if (!isDragging) return;
+  touchY = e.clientY || e.touches[0].clientY;
+  scrollY += (touchY - touchStart) * 2.5;
+  touchStart = touchY;
+};
+const handleTouchEnd = () => {
+  isDragging = false;
+  $menu.classList.remove("is-dragging");
+};
+
+/*--------------------
+Listeners
+--------------------*/
+$menu.addEventListener("mousewheel", handleMouseWheel);
+
+$menu.addEventListener("touchstart", handleTouchStart);
+$menu.addEventListener("touchmove", handleTouchMove);
+$menu.addEventListener("touchend", handleTouchEnd);
+
+$menu.addEventListener("mousedown", handleTouchStart);
+$menu.addEventListener("mousemove", handleTouchMove);
+$menu.addEventListener("mouseleave", handleTouchEnd);
+$menu.addEventListener("mouseup", handleTouchEnd);
+
+$menu.addEventListener("selectstart", () => {
+  return false;
+});
+
+/*--------------------
+Resize
+--------------------*/
+window.addEventListener("resize", () => {
+  menuHeight = $menu.clientHeight;
+  itemHeight = $items[0].clientHeight;
+  wrapHeight = $items.length * itemHeight;
+});
+
+/*--------------------
+Render
+--------------------*/
+const render = () => {
+  requestAnimationFrame(render);
+  y = lerp(y, scrollY, 0.1);
+  dispose(y);
+
+  scrollSpeed = y - oldScrollY;
+  oldScrollY = y;
+
+  gsap.to($items, {
+    scale: 1 - Math.min(100, Math.abs(scrollSpeed)) * 0.005,
+    rotate: scrollSpeed * 0.2,
+  });
+};
+render();
+
+// document.addEventListener("mousemove", parallax);
+
+// function parallax(e) {
+//   // console.log("e is:");
+//   // console.log(e.pageX, e.pageY);
+//   const elements = document.querySelectorAll(".move");
+
+//   elements.forEach((layer) => {
+//     const speed = 2;
+
+//     const x = window.innerWidth - (e.pageX * speed) / 2;
+//     const y = window.innerHeight - (e.pageY * speed) / 2;
+
+//     console.log(`x is ${x}`);
+//     console.log(`y is ${y}`);
+
+//     // layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+//     gsap.set(".move", { x: `${x}`, y: `${y}` });
+//   });
+// }
--- assets/js/app.js
+++ assets/js/app.js
@@ -4,38 +4,58 @@ import i2 from "../images/2.jpg";
 import i3 from "../images/3.jpg";
 import i4 from "../images/4.jpg";
 
+var images = [i0, i1, i2, i3, i4];
+var content = document.getElementById("content");
+var popup = document.querySelector(".popup");
+var close = document.querySelector(".close");
 
-var images = [i0,i1,i2,i3,i4]
-var content = document.getElementById('content');
-var popup = document.querySelector('.popup')
-var close = document.querySelector('.close')
-
-for(let i=0;i<50;i++){
-    let story = document.createElement("div");
-    // let marquee = document.createElement("marquee")
-    // marquee.innerText("This is basic example of marquee")
-    // story.appendChild(marquee)
-    story.id = `c${i}`
-    story.innerHTML=`<marquee direction="${i%2?'left':'right'}" class="no-select">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore totam aliquam ex a voluptatem sit quae, quaerat, numquam incidunt voluptates, ut accusamus! Inventore, soluta est?                                                                             </marquee>`
-    story.classList.add("story")
-    story.classList.add(`w${i%5}`)
-    content.appendChild(story)
+for (let i = 0; i < 50; i++) {
+  let story = document.createElement("div");
+  // let marquee = document.createElement("marquee")
+  // marquee.innerText("This is basic example of marquee")
+  // story.appendChild(marquee)
+  story.id = `c${i}`;
+  story.innerHTML = `<marquee direction="${
+    i % 2 ? "left" : "right"
+  }" class="no-select">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore totam aliquam ex a voluptatem sit quae, quaerat, numquam incidunt voluptates, ut accusamus! Inventore, soluta est?                                                                             </marquee>`;
+  story.classList.add("story");
+  story.classList.add(`w${i % 5}`);
+  content.appendChild(story);
 }
 
-content.addEventListener("click",function(e){
-    if (e.path[0].id[1]==='o'){
-        return;
-    }
-    popup.classList.remove("hide")
-    popup.classList.add("show");
-    console.log(e.path[0].id[1])
-    popup.querySelector('img').setAttribute('src',`${images[e.path[0].id.slice(1)%5]}.jpg`)
-    // console.log(popup.querySelector('img'))
-})
+content.addEventListener("click", function (e) {
+  if (e.path[0].id[1] === "o") {
+    return;
+  }
+  popup.classList.remove("hide");
+  popup.classList.add("show");
+  console.log(e.path[0].id[1]);
+  popup
+    .querySelector("img")
+    .setAttribute("src", `${images[e.path[0].id.slice(1) % 5]}.jpg`);
+  // console.log(popup.querySelector('img'))
+});
 
-close.addEventListener("click",function(e){
-    popup.classList.remove("show");
-    popup.classList.add("hide")
-})
+close.addEventListener("click", function (e) {
+  popup.classList.remove("show");
+  popup.classList.add("hide");
+});
 
 // console.log('d')
+
+document.addEventListener("mousemove", parallax);
+
+function parallax(e) {
+  // console.log("e is:");
+  // console.log(e.pageX, e.pageY);
+  const elements = document.querySelectorAll(".move");
+
+  elements.forEach((layer) => {
+    const speed = 2;
+
+    const x = (window.innerWidth - e.pageX * speed) / 100;
+    const y = (window.innerHeight - e.pageY * speed) / 100;
+
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
+}
